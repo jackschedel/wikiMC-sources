88b77089c6f914f6cfce88ddf331064ec5266a5c
Jack Schedel <jackschedel@gmail.com>
2026-06-18 17:27:40 -0400

    Initial commit with Monifactory icons

---
+++ quest-sources.config.mjs
@@ -0,0 +1,19 @@
+export default [
+  {
+    slug: "Monifactory",
+    repo: "ThePansmith/Monifactory",
+    branch: "main",
+    questsPaths: [
+      "config/ftbquests/quests",
+      "config-overrides/normal/ftbquests/quests",
+    ],
+    langDirs: ["kubejs/assets/ftbquests/lang"],
+  },
+  {
+    slug: "GregTech-Modern-Community-Pack",
+    repo: "GregTechCEu/GregTech-Modern-Community-Pack",
+    branch: "main",
+    questsPaths: ["config/ftbquests/quests"],
+    langDirs: ["kubejs/assets/gtceu/lang"],
+  },
+];
